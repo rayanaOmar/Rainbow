dbc56c766db5a7ee3df08a51be59f0d2c2e10a86
rayanaOmar <rayanaomar_@hotmail.com>
2021-12-12 17:17:36 +0000

    Rainbow

--- Rainbow/ViewController.swift
+++ Rainbow/ViewController.swift
@@ -9,11 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    let colors:[UIColor] = [.red, .orange, .yellow , .green,.blue, .purple]
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
         // Do any additional setup after loading the view.
+    }
+}
+
+extension ViewController: UITableViewDataSource, UITableViewDelegate{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        colors.count
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat{
+        tableView.frame.height/6
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
+        cell.backgroundColor = colors[indexPath.row]
+        return cell
+    }
     }
 
 
-}
+
+
+
+
+
+
+
+
+
+
 
